09bdd6d33bfd998c833e4a6ead71526b1375b3b4
Azka Alif <azkaganteng50@gmail.com>
2024-12-13 14:47:15 +0700

    --

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,10 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
-    
-
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <script src="{{ asset('js/jquery-3.7.1.js') }}"></script>
 </head>
 <body>
     <header>
@@ -23,6 +20,7 @@
     </footer>
 
     <script src="{{ asset('js/app.js') }}"></script>
+    s
     
 
     
