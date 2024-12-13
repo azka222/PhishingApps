84d02f62955fbebbf571fbcdeb3f7d26399ab3e4
Azka Alif <azkaganteng50@gmail.com>
2024-12-13 10:53:22 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,11 @@
 use App\Http\Controllers\AuthenticateController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/login', [AuthenticateController::class, 'loginView'])->name('login');
 
 Route::group(['middleware' => 'auth'], function () {
-    Route::get('/dashboard', function () {
+    Route::get('/', function () {
         return view('contents.dashboard');
     })->name('dashboard');
 });
