0e629b82e46b15997d5feb96e5bf15a8c3ece5a8
Azka Alif <azkaganteng50@gmail.com>
2024-12-13 09:40:14 +0700

    configure middleware

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+// $app->router->get('/', function () {
+//     return redirect('/login'); // Redirect ke halaman login
+// })->middleware('auth');
+
+// $app->router->get('/login', function () {
+//     return view('auth.login'); // Halaman login
+// });
+
 Route::get('/', function () {
-    return view('welcome');
-});
+    return redirect('/login'); 
+})->middleware('auth');
