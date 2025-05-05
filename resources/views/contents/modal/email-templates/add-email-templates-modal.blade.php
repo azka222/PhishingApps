b8d62138d5c8674e0b4f57a87e5255a2d3250aed
Arvyn <arvyn.bang@gmail.com>
2025-05-05 22:16:55 +0700

    change name button

--- resources/views/contents/modal/email-templates/add-email-templates-modal.blade.php
+++ resources/views/contents/modal/email-templates/add-email-templates-modal.blade.php
@@ -148,7 +148,7 @@
             <div class="flex items-center justify-between p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600 gap-2">
                 <div class="flex items-center">
                     <button data-modal-hide="static-modal" type="button"
-                        class="text-white bg-yellow-700 hover:bg-yellow-800 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-xs md:text-sm px-5 py-2.5 text-center dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800"><a href="{{ route('downloadEmailAttachment') }}">Attachment</a></button>
+                        class="text-white bg-yellow-700 hover:bg-yellow-800 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-xs md:text-sm px-5 py-2.5 text-center dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800"><a href="{{ route('downloadEmailAttachment') }}">Download Variable Guide </a></button>
                 </div>
                 <div
                     class="flex items-center justify-end gap-2">
