07e77eaea3146110f0203fd75fa7e71f11fc1e95
Azka Alif <azkaganteng50@gmail.com>
2025-05-14 16:09:25 +0700

    Update showAllEmployee.blade.php

--- resources/views/contents/modal/dashboard/showAllEmployee.blade.php
+++ resources/views/contents/modal/dashboard/showAllEmployee.blade.php
@@ -54,7 +54,7 @@
                 <div class="flex justify-end items-center mt-5 gap-2">
                         <button type="button"
                             onclick="hideModal('show-all-employee-modal');showModal('import-target-modal')"
-                            class="focus:outline-none text-black bg-gray-100 hover:bg-gray-300 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xs px-5 py-2.5 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Back</button>
+                            class="py-2.5 px-5 ms-3 text-xs md:text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Back</button>
                 </div>
             </form>
         </div>
