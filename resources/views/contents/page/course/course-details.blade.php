4a0351d4a8761686470aac41f6ce2abc7c468829
Arvyn <arvyn.bang@gmail.com>
2025-05-19 20:48:07 +0700

    light mode course

--- resources/views/contents/page/course/course-details.blade.php
+++ resources/views/contents/page/course/course-details.blade.php
@@ -43,10 +43,10 @@
                     </div>
 
                     <button id="startButton" onclick="checkAgreement()"
-                        class="px-4 py-2 text-sm md:text-xs font-medium text-white rounded-xl 
+                        class="px-4 py-2 text-sm md:text-xs font-medium text-white rounded-xl
                            bg-green-600 dark:bg-green-500 w-full md:w-32
-                           hover:bg-green-700 dark:hover:bg-green-600 
-                           disabled:bg-gray-400 
+                           hover:bg-green-700 dark:hover:bg-green-600
+                           disabled:bg-gray-400
                            disabled:text-gray-200 transition-colors duration-200">
                         <span class="">Start</span>
                     </button>
@@ -59,9 +59,9 @@
         <div class="xl:hidden flex w-full p-4 justify-end">
             <div class="">
                 <button id="submitButton" onclick="checkAllQuizAnswered()"
-                    class="px-4 py-2 text-md md:text-sm font-medium text-white rounded-xl 
-                       bg-green-600 dark:bg-green-500 
-                       hover:bg-green-700 dark:hover:bg-green-600 
+                    class="px-4 py-2 text-md md:text-sm font-medium text-white rounded-xl
+                       bg-green-600 dark:bg-green-500
+                       hover:bg-green-700 dark:hover:bg-green-600
                     ">
                     <span class="text-xs md:text-sm">Submit</span>
                 </button>
@@ -81,9 +81,9 @@
         <div class="w-1/4 p-4 hidden xl:flex justify-end">
             <div class="">
                 <button id="submitButton" onclick="checkAllQuizAnswered()"
-                    class="px-4 py-2 text-md md:text-sm font-medium text-white rounded-xl 
-                       bg-green-600 dark:bg-green-500 
-                       hover:bg-green-700 dark:hover:bg-green-600 
+                    class="px-4 py-2 text-md md:text-sm font-medium text-white rounded-xl
+                       bg-green-600 dark:bg-green-500
+                       hover:bg-green-700 dark:hover:bg-green-600
                     ">
                     <span class="text-xs md:text-sm">Submit</span>
                 </button>
@@ -217,9 +217,10 @@
             $(".need-to-hide").hide();
             $("#content-" + order).show();
             let buttonNumber = document.getElementById("button-number-" + order);
-            $(".number-section").removeClass("bg-green-600").addClass("bg-gray-700");
-            buttonNumber.classList.add("bg-green-600");
-            buttonNumber.classList.remove("bg-gray-700");
+            $(".number-section").removeClass("bg-green-600 text-white").addClass("bg-gray-500 text-white");
+            // Aktifkan warna tombol yang dipilih
+            buttonNumber.classList.add("bg-green-600", "text-white");
+            buttonNumber.classList.remove("bg-gray-500", "text-black");
         }
 
         function getCourse() {
@@ -246,37 +247,37 @@
                         let button = '';
                         if (order == firsOrder) {
                             button = ` <button id="" onclick="nextRender(${order})"
-                                            class="px-4 py-2 text-md md:text-sm font-medium text-white rounded-xl 
-                                        bg-green-600 dark:bg-green-500 
-                                        hover:bg-green-700 dark:hover:bg-green-600 
-                                        disabled:bg-gray-400 
+                                            class="px-4 py-2 text-md md:text-sm font-medium text-white rounded-xl
+                                        bg-green-600 dark:bg-green-500
+                                        hover:bg-green-700 dark:hover:bg-green-600
+                                        disabled:bg-gray-400
                                         disabled:text-gray-200 transition-colors duration-200">
                                             <span class="text-xs md:text-sm">Next</span>
                                         </button>`;
                         } else if (order == lastOrder) {
                             button = `<button id="" onclick="previousRender(${order})"
-                                            class="px-4 py-2 text-md md:text-sm font-medium text-white rounded-xl 
-                                    bg-blue-600 dark:bg-blue-500 
-                                    hover:bg-blue-700 dark:hover:bg-blue-600 
-                                    disabled:bg-gray-400 
+                                            class="px-4 py-2 text-md md:text-sm font-medium text-white rounded-xl
+                                    bg-blue-600 dark:bg-blue-500
+                                    hover:bg-blue-700 dark:hover:bg-blue-600
+                                    disabled:bg-gray-400
                                     disabled:text-gray-200 transition-colors duration-200">
                                             <span class="text-xs md:text-sm">Previous</span>
                                         </button>`;
                         } else {
                             button = `
                                         <button id="" onclick="previousRender(${order})"
-                                            class="px-4 py-2 text-md md:text-sm font-medium text-white rounded-xl 
-                                    bg-blue-600 dark:bg-blue-500 
-                                    hover:bg-blue-700 dark:hover:bg-blue-600 
-                                    disabled:bg-gray-400 
+                                            class="px-4 py-2 text-md md:text-sm font-medium text-white rounded-xl
+                                    bg-blue-600 dark:bg-blue-500
+                                    hover:bg-blue-700 dark:hover:bg-blue-600
+                                    disabled:bg-gray-400
                                     disabled:text-gray-200 transition-colors duration-200">
                                             <span class="text-xs md:text-sm">Previous</span>
                                         </button>
                                          <button id="" onclick="nextRender(${order})"
-                                            class="px-4 py-2 text-md md:text-sm font-medium text-white rounded-xl 
-                                        bg-green-600 dark:bg-green-500 
-                                        hover:bg-green-700 dark:hover:bg-green-600 
-                                        disabled:bg-gray-400 
+                                            class="px-4 py-2 text-md md:text-sm font-medium text-white rounded-xl
+                                        bg-green-600 dark:bg-green-500
+                                        hover:bg-green-700 dark:hover:bg-green-600
+                                        disabled:bg-gray-400
                                         disabled:text-gray-200 transition-colors duration-200">
                                             <span class="text-xs md:text-sm">Next</span>
                                         </button>`;
@@ -299,15 +300,15 @@
                                            ${course.model.content}
                                         </h2>
                                     </div>
-                                    
+
                                     <div class="flex items-center justify-end mt-8 gap-2">
                                         ${button}
-                                      
+
                                     </div>
                                 </div>`;
 
                             let buttonNumber = `<button onclick="goToNumber(${order})" class="4xl:col-span-1 col-span-2">
-                                    <div id="button-number-${order}" class="number-section text-xs md:text-sm bg-gray-700 p-4 rounded-lg dark:hover:bg-green-600">
+                                    <div id="button-number-${order}" class="number-section text-xs md:text-sm bg-gray-500 p-4 rounded-lg dark:hover:bg-green-600">
                                         Material ${order}
                                     </div>
                                 </button>`
@@ -321,7 +322,7 @@
                                 .group);
                             options = shuffleArray(options);
                             let quiz = ` <div id="content-${order}"
-                                            class="need-to-hide w-full md:p-8 p-2 dark:bg-gray-700   border-2 dark:border-gray-500 border-gray-800 rounded-xl">
+                                            class="need-to-hide w-full md:p-8 p-2 dark:bg-gray-500   border-2 dark:border-gray-500 border-gray-800 rounded-xl">
                                             <div class="flex  items-center justify-center mb-4">
                                                 <h1 class="md:text-3xl text-xl font-semibold" id="">${course.model.title}</h1>
                                             </div>
@@ -334,7 +335,7 @@
                                                 </h2>
                                             </div>
                                             <div id="option-section-${order}" class="flex flex-col  gap-4 mt-8">
-                                            
+
                                             </div>
                                             <div class="flex items-center justify-end mt-8 gap-2">
                                                ${button}
@@ -342,7 +343,7 @@
                                         </div>`;
 
                             let buttonNumber = `<button onclick="goToNumber(${order})" class="4xl:col-span-1 col-span-2">
-                                    <div id="button-number-${order}" class="number-section text-xs md:text-sm  bg-gray-700 p-4 rounded-lg dark:hover:bg-green-600">
+                                    <div id="button-number-${order}" class="number-section text-xs md:text-sm  bg-gray-500 p-4 rounded-lg dark:hover:bg-green-600">
                                         Quiz ${order}
                                     </div>
                                 </button>`
